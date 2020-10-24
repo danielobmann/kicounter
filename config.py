4f423886a4212ba610d38ebbd951634377dbff76
dansenob <obmann.daniel@live.de>
2020-10-24 14:46:59 +0200

    UPDATE: config now creates necessary paths

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
-# -----------------
-# Global variables
+import os
 
 vars = {"path": "data/kicount_old_middleJuly.csv",
+        "model_path": "models/",
+        "img_path": "images/",
         "mins": 24 * 60,
         "minimum": 0,
         "maximum": 350,
         "opening": 9,
         "closing": 22}
+
+if not os.path.exists(vars["model_path"]):
+        print("Creating model path.")
+        os.mkdir(vars["model_path"])
+
+if not os.path.exists(vars["img_path"]):
+        print("Creating image path.")
+        os.mkdir(vars["img_path"])
